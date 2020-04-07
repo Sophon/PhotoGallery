9b48d4fcd2a9859d7bbeeb93e2df09d22282728b
Le Duy Anh <lenduya@gmail.com>
2020-04-07 23:36:48 +0200

    Implement fetchInterestingPhotos()

--- app/src/main/java/com/bignerdranch/android/photogallery/retrofit/FlickrRepository.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/retrofit/FlickrRepository.kt
@@ -4,16 +4,17 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.bignerdranch.android.photogallery.api.flickr.FlickrApi
 import com.bignerdranch.android.photogallery.api.flickr.FlickrResponse
+import com.bignerdranch.android.photogallery.api.flickr.PhotoResponse
 import com.bignerdranch.android.photogallery.model.GalleryItem
-import retrofit2.Retrofit
+import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
-import retrofit2.create
 
 private const val TAG = "FlickrRepository"
 
 class FlickrRepository {
     //region Private vars
     private val flickrApi: FlickrApi
+    private lateinit var flickrRequest: Call<FlickrResponse>
     //endregion
 
     init {
@@ -24,4 +25,34 @@ class FlickrRepository {
 
         flickrApi = retrofit.create(FlickrApi::class.java)
     }
+
+    //region Public funs
+    fun fetchInterestingPhotos()
+        : LiveData<List<GalleryItem>> {
+        val responseLiveData: MutableLiveData<List<GalleryItem>> = MutableLiveData()
+        flickrRequest = flickrApi.fetchInterestingness()
+
+        flickrRequest.enqueue(
+            object: Callback<FlickrResponse> {
+                override fun onFailure(call: Call<FlickrResponse>, t: Throwable) {
+                    TODO("Not yet implemented")
+                }
+
+                override fun onResponse(
+                    call: Call<FlickrResponse>,
+                    response: Response<FlickrResponse>
+                ) {
+                    val photoResponse: PhotoResponse? = response.body()?.photos
+                    var galleryItems: List<GalleryItem> =
+                        photoResponse?.galleryItems ?: mutableListOf()
+                    galleryItems = galleryItems.filterNot { it.url.isBlank() }
+
+                    responseLiveData.value = galleryItems
+                }
+            }
+        )
+
+        return responseLiveData
+    }
+    //endregion
 }
