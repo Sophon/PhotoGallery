d48fe8fc28c2e2ed63b8bb03013c0c0f9afe181b
Le Duy Anh <lenduya@gmail.com>
2020-04-08 01:15:45 +0200

    Connect PhotoGalleryFragment to activity

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
@@ -3,6 +3,7 @@ package com.bignerdranch.android.photogallery
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.bignerdranch.android.photogallery.databinding.ActivityPhotoGalleryBinding
+import com.bignerdranch.android.photogallery.view.gallery.PhotoGalleryFragment
 
 private lateinit var binding: ActivityPhotoGalleryBinding
 
@@ -15,6 +16,14 @@ class PhotoGalleryActivity : AppCompatActivity() {
         binding = ActivityPhotoGalleryBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        val isFragmentContainerEmpty = (savedInstanceState == null)
+        if(isFragmentContainerEmpty) {
+            supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragmentContainer, PhotoGalleryFragment())
+                .commit()
+        }
     }
     //endregion
 }
