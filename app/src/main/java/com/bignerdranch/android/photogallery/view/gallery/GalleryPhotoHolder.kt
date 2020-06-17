ea8df4d3142a31bbf1bfbfd55173ef2a75365e48
Le Duy Anh <lenduya@gmail.com>
2020-06-17 21:46:50 +0200

    v1.0

--- app/src/main/java/com/bignerdranch/android/photogallery/view/gallery/GalleryPhotoHolder.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/view/gallery/GalleryPhotoHolder.kt
@@ -1,19 +1,39 @@
 package com.bignerdranch.android.photogallery.view.gallery
 
-import android.graphics.drawable.Drawable
-import android.widget.ImageView
+import android.content.Context
+import android.view.View
 import androidx.appcompat.widget.AppCompatImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.databinding.ListItemGalleryBinding
 import com.bignerdranch.android.photogallery.model.GalleryItem
+import com.bignerdranch.android.photogallery.view.webContent.PhotoPageActivity
 import com.squareup.picasso.Picasso
+import timber.log.Timber
 
-internal class PhotoHolder(binding: ListItemGalleryBinding)
-    : RecyclerView.ViewHolder(binding.root) {
+internal class PhotoHolder(
+    private val context: Context,
+    binding: ListItemGalleryBinding
+) : RecyclerView.ViewHolder(binding.root), View.OnClickListener {
 
-    private val imageView:AppCompatImageView = binding.galleryItemImagView
+    //region Private vars
+    private val imageView: AppCompatImageView = binding.galleryItemImageView
+    private lateinit var galleryItem: GalleryItem
+    //endregion
+
+    init {
+        itemView.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        Timber.d("Photo: clicked on a photo -> ${galleryItem.photoPageUri}")
+
+        val intent = PhotoPageActivity.newIntent(context, galleryItem.photoPageUri)
+        context.startActivity(intent)
+    }
 
     fun bind(galleryItem: GalleryItem) {
+        this.galleryItem = galleryItem
+
         Picasso.get()
             .load(galleryItem.url)
             .into(imageView)
