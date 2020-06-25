83296367c7a3830ec3ced1a9be90758d791398a9
Le Duy Anh <lenduya@gmail.com>
2020-06-25 14:29:10 +0200

    Create viewModel for PhotoPage

--- app/src/main/java/com/bignerdranch/android/photogallery/view/webContent/PhotoPageViewModel.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/view/webContent/PhotoPageViewModel.kt
@@ -1,9 +1,14 @@
 package com.bignerdranch.android.photogallery.view.webContent
 
 import androidx.lifecycle.ViewModel
+import com.bignerdranch.android.photogallery.model.GalleryItem
 import com.bignerdranch.android.photogallery.retrofit.PhotoRepository
 
 class PhotoPageViewModel: ViewModel() {
 
     private val photoRepository = PhotoRepository.get()
+
+    fun savePhoto(galleryItem: GalleryItem) {
+        photoRepository.addGalleryItem(galleryItem)
+    }
 }
