11c60bb41f7e03b44b7203f8f251e3df0ba41ed6
Le Duy Anh <lenduya@gmail.com>
2020-07-05 01:16:08 +0200

    Change PhotoAdapter to ListAdapter<>

--- app/src/main/java/com/bignerdranch/android/photogallery/view/gallery/GalleryPhotoAdapter.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/view/gallery/GalleryPhotoAdapter.kt
@@ -3,16 +3,16 @@ package com.bignerdranch.android.photogallery.view.gallery
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
-import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
 import com.bignerdranch.android.photogallery.databinding.ListItemGalleryBinding
 import com.bignerdranch.android.photogallery.model.GalleryItem
 
 internal class PhotoAdapter(private val context: Context, private val galleryItems: List<GalleryItem>)
-    : RecyclerView.Adapter<PhotoHolder>() {
+    : ListAdapter<GalleryItem, PhotoHolder>(DiffCallback()) {
 
     private lateinit var binding: ListItemGalleryBinding
 
-    //region Overrides
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
         binding = ListItemGalleryBinding.inflate(
             LayoutInflater.from(parent.context),
@@ -24,11 +24,19 @@ internal class PhotoAdapter(private val context: Context, private val galleryIte
     }
 
     override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
-        val galleryItem: GalleryItem = galleryItems[position]
+        val galleryItem = galleryItems[position]
 
         holder.bind(galleryItem)
     }
+}
 
-    override fun getItemCount(): Int = galleryItems.count()
-    //endregion
+internal class DiffCallback: DiffUtil.ItemCallback<GalleryItem>() {
+
+    override fun areItemsTheSame(oldItem: GalleryItem, newItem: GalleryItem): Boolean {
+        return oldItem.id == newItem.id
+    }
+
+    override fun areContentsTheSame(oldItem: GalleryItem, newItem: GalleryItem): Boolean {
+        return areItemsTheSame(oldItem, newItem)
+    }
 }
