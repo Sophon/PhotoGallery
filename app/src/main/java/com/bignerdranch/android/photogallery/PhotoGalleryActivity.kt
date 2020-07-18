c4d07ff1910b72f98791aa5140c12ab0cc0101ed
Le Duy Anh <lenduya@gmail.com>
2020-07-19 00:43:20 +0200

    Launch gallery fragment with correct gallery type.

--- app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
+++ app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
@@ -4,12 +4,31 @@ import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import com.bignerdranch.android.photogallery.databinding.ActivityPhotoGalleryBinding
+import com.bignerdranch.android.photogallery.model.GalleryType
 import com.bignerdranch.android.photogallery.view.gallery.PhotoGalleryFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 private lateinit var binding: ActivityPhotoGalleryBinding
 
 class PhotoGalleryActivity : AppCompatActivity() {
+
+    private val galleryType = MutableLiveData<GalleryType>(GalleryType.ONLINE)
+
+    private val bottomNavItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when(item.itemId) {
+            R.id.botNav_web -> {
+                galleryType.value = GalleryType.ONLINE
+            }
+            else -> {
+                galleryType.value = GalleryType.FAVORITES
+            }
+        }
+
+        true
+    }
 
     companion object {
         fun newIntent(context: Context): Intent {
@@ -25,13 +44,23 @@ class PhotoGalleryActivity : AppCompatActivity() {
         val view = binding.root
         setContentView(view)
 
-        val isFragmentContainerEmpty = (savedInstanceState == null)
-        if(isFragmentContainerEmpty) {
-            supportFragmentManager
-                .beginTransaction()
-                .add(R.id.fragmentContainer, PhotoGalleryFragment())
-                .commit()
-        }
+//        val isFragmentContainerEmpty = (savedInstanceState == null)
+//        if(isFragmentContainerEmpty) {
+//            supportFragmentManager
+//                .beginTransaction()
+//                .add(R.id.fragmentContainer, PhotoGalleryFragment(galleryType))
+//                .commit()
+//        }
+
+        galleryType.observe(
+            this,
+            Observer { galleryType ->
+                supportFragmentManager
+                    .beginTransaction()
+                    .add(R.id.fragmentContainer, PhotoGalleryFragment(galleryType))
+                    .commit()
+            }
+        )
     }
     //endregion
 }
